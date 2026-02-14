5553bd7cc0eedcd07330095ce02fa1b8ebb30bdc
wouza1204 <niemann.adam@gmail.com>
2026-02-14 15:38:41 +0100

    Update script.js

--- Valentine-proposal-main/script.js
+++ Valentine-proposal-main/script.js
@@ -117,7 +117,7 @@ function showMessage(response) {
 
     // Update the text content, display the message, and change the image to "dance.gif"
     const yesMessage = document.getElementById("question");
-    yesMessage.textContent = "See you on the 14th my princess";
+    yesMessage.textContent = "Wir sehen uns heute um 18 Uhr Babe, zieh dich schön an:)";
     yesMessage.style.display = "block";
     yesMessage.style.fontStyle = "normal";
     document.getElementsByClassName("image")[0].src = "images/dance.gif";
